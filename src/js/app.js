a2022f52898874cd68959d61df7eb140c26ef989
Krista Calderon <kamsci@gmail.com>
2016-06-04 11:43:11 -0700

    Update: columns > rows, .html > .append; Built: board that changes size with levels

--- src/js/app.js
+++ src/js/app.js
@@ -26,14 +26,15 @@ var numberOfMonsters = monstersArray.length;
 console.log("numberOFMonsters: ", numberOfMonsters);
 var levelsArray = [3, 4, 5];
 var currentLevel = 0;
+var gameArray = createGameArray();
 
 // CREATE GAME LEVEL
 // F:createGameArray: create 2D array with random monsters using F:createRow
 function createGameArray () {
   var localArray = [];
   for (var i = 0; i < levelsArray[currentLevel]; i++) {
-  var rowArray = createRow();
-  localArray.push(rowArray);
+    var rowArray = createRow();
+    localArray.push(rowArray);
   }
   return localArray;
 }
@@ -57,22 +58,44 @@ function createRandomM() {
 }
 
 // Process Outline
-var gameArray = createGameArray();
-console.log("gameArray: ", gameArray);
+
+// console.log("gameArray: ", gameArray);
 
 // CREATE GAME BOARD
 // F:createDivRows: HTML number of rows = current level
 //  Add css styles for rows
 function createDivRows(array) {
   array.forEach(function(e) {
-    $('#board').html("<div class='row'></div>")
-    // add class
+    console.log("e:", e);
+    $("#board").append("<div class='row'></div>");
   });
 }
-createDivRows(gameArray);
+
+
+
 // F:createDivBoxes: HTML boxes per row = current level
+function createDivBoxes(array) {
+  localArray = array[0];
+  localArray.forEach(function() {
+    $(".row").append("<div class='box'></div>");
+  });
+}
+    // $(".row").appendTo("<div class='box'></div>");
+    // // $(".box").attr("id", id);
+    // $(".box").html(array[i]);
+    // console.log("boxArray.length: ", array.length);
+    // console.log("boxArray: ", array);
+
 //  Add box IDs 1 thru totalboxes
 //  Add css styles for each monster
 //  Add css styles for boxes
+
+// RUN GAMEBOARD
+function runGameBoard(array) {
+  createDivRows(array);
+  createDivBoxes(array);
+}
+
+runGameBoard(gameArray);
 
 }); // End Ready
