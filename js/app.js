5248d8eeba793c408be143122d33a4402fb2a055
Krista Calderon <kamsci@gmail.com>
2016-06-05 20:40:30 -0700

    REFACTOR:started over with different design; divs identified with array possitions as box ids, but not in html; gameArray created

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
   });
 //
 // GLOBAL VARIABLES
-var monstersArray = [
+var monstersObjArray = [
   {
     "symbol": "S",
     "cssClass": "circle"
@@ -21,117 +21,189 @@ var monstersArray = [
     "cssClass": "triangle"
   }
 ];
-var numberOfMonsters = monstersArray.length;
+var numberOfMonsters = monstersObjArray.length;
 // console.log("numberOFMonsters: ", numberOfMonsters);
 var levelsArray = [3, 4, 5];
 var currentLevel = 0;
 // CREATE & STORE gameArray GLOBALLY
-var gameArray = createGameArray();
-console.log(gameArray);
+var gameArray = setUpBoard();
+console.log("gameArray: ", gameArray);
+// CREATE STORAGE FOR singleArray GLOBALLY
+var singleGamePieceArray = [];
 //
 // CREATE GAME PIECES BY LEVEL
 // F:createGameArray: create 2D array with random monsters using F:createRow
-function createGameArray () {
+
+function setUpBoard() {
+  // create swap piece
+  // create temp array
   var localArray = [];
+  // each row
   for (var i = 0; i < levelsArray[currentLevel]; i++) {
-    var rowArray = createRow();
-    localArray.push(rowArray);
+    // create row and push to DOM
+    $("#board").append("<div class='row' id='row" + i + "'></div>");
+    for (var k = 0; k < levelsArray[currentLevel]; k++) {
+      // create random monster
+      var localMonsterIdx = createRandomMonster();
+      // add monster to gameArray at position i, k
+      localArray.push(monstersObjArray[localMonsterIdx]);
+      // create box and push to row
+      var box = $("<div class='box' id='" + i + k + "'></div>");
+      console.log("box: ", box);
+      // add click listeners - create fct parameters i, k
+      // $(".box").click(function () {
+      //   swapPiece(i, k);
+      // });
+    }
+    $("#row" + i).append(box);
   }
   return localArray;
 }
-// F:createRow: create array of rows with random monsters
-function createRow() {
-  var localArray = [];
-  var localRows = levelsArray[currentLevel];
-  // console.log("localRows: ", localRows);
-  for (var i = 0; i < localRows; i++) {
-    var localMonsterIdx = createRandomM();
-    // console.log("localMonsterIdx: ", localMonsterIdx);
-    localArray.push(monstersArray[localMonsterIdx].symbol);
-  }
-  return localArray;
-}
+
+// function swapPiece(row, col) {
+//   var tempPiece = gameArray[row][col];
+//   gameArray[row][col] = swapPiece;()
+//   swapPiece = tempPiece;
+// }
 //
 // CREATE RANDOM MONSTER
-// F:createRandomM: create 1 of 3 random monsters
-function createRandomM() {
+// F:createRandomMonster: create 1 of 3 random monsters
+function createRandomMonster() {
 // Random number logic
   return Math.floor(Math.random() * numberOfMonsters);
 }
-//
-// CREATE BLANK GAME BOARD
-// F:createDivRows: HTML number of rows = current level
-function createDivRows(array) {
-  $(array).each(function() {
-    $("#board").append("<div class='row'></div>");
-  });
-}
-// F:createDivBoxes: HTML boxes per row = current level
-function createDivBoxes(array) {
-  localArray = array[0];
-  $(localArray).each(function() {
-    $(".row").append("<div class='box'></div>");
-  });
-}
-//
-// COMBINE GAME BOARD + GAME PIECES
-// F:createSingleGamePieceArray
-function createSingleGamePieceArray(array) {
-  localArray = [];
-  $(array).each(function(idx, element) {
-    // // console.log("idx: ", idx);
-    $.merge(localArray, element);
-  });
-  // console.log("localArray: ", localArray);
-  return localArray;
-}
-// F:addGamePiecesToHtml
-function addGamePiecesToHtml(array) {
-  $(".box").each(function(idx, e) {
-    $(e).attr("id", idx);
-    $(e).html(array[idx]);
-  });
-}
-//
 // CREATE SWITCH PIECE
 function createSwitchPiece() {
-  var localMonsterIdx = createRandomM();
-  var switchPiece = monstersArray[localMonsterIdx].symbol;
+  var localMonsterIdx = createRandomMonster();
+  var switchPiece = monstersObjArray[localMonsterIdx];
   $("#switch-piece").html(switchPiece);
   return switchPiece;
 }
 //
-// RUN GAMEBOARD
-function runGameBoard(array) {
-  // CREATE BLANK GAME BOARD
-  createDivRows(array);
-  createDivBoxes(array);
-  // COMBINE GAME BOARD + GAME PIECES
-  var  singleGamePieceArray = createSingleGamePieceArray(array);
-  addGamePiecesToHtml(singleGamePieceArray);
-
-}
-//
-// CALL RUN GAME BOARD
-runGameBoard(gameArray);
-//
 // CREATE & STORE currentSwitchPiece GLOBALLY
 var currentSwitchPiece = createSwitchPiece();
+console.log("SwitchP: ", currentSwitchPiece);
 //
+// RUN GAME
+function runGame () {
+  createSwitchPiece();
+}
+
+// function createGameArray () {
+//   var localArray = [];
+//   for (var i = 0; i < levelsArray[currentLevel]; i++) {
+//     var rowArray = createRow();
+//     localArray.push(rowArray);
+//   }
+//   return localArray;
+// }
+// // F:createRow: create array of rows with random monsters
+// function createRow() {
+//   var localArray = [];
+//   var localRows = levelsArray[currentLevel];
+//   for (var i = 0; i < localRows; i++) {
+//     var localMonsterIdx = createRandomMonster();
+//     // console.log("localMonsterIdx: ", localMonsterIdx);
+//     localArray.push(monstersObjArray[localMonsterIdx]);
+//   }
+//   return localArray;
+// }
+// //
+// //
+// // CREATE BLANK GAME BOARD
+// // F:createDivRows: HTML number of rows = current level
+// function createDivRows(array) {
+//   localId = 1;
+//   $(array).each(function() {
+//     $("#board").append("<ul class='rows'></ul>");
+//   });
+//   $("ul").each(function() {
+//     $(this).addClass("row" + localId);
+//     localId++;
+//   });
+// }
+// // F:createDivBoxes: HTML boxes per row = current level
+// function createDivBoxes(array) {
+//   localArray = array[0];
+//   $(localArray).each(function() {
+//     $(".rows").append("<li class='box' onclick='alert(\"\")'></li>");
+//   });
+// }
+// //
+// // COMBINE GAME BOARD + GAME PIECES
+// // F:createSingleGamePieceArray
+// function createSingleGamePieceArray(array) {
+//   localArray = [];
+//   $(array).each(function(idx, element) {
+//     // // console.log("idx: ", idx);
+//     $.merge(localArray, element);
+//   });
+//   // console.log("localArray: ", localArray);
+//   return localArray;
+// }
+// // // F:addGamePiecesToHtml
+// // function addGamePiecesToHtml(array) {
+// //   $(".box").each(function(idx, e) {
+
+// //     $(e).html(array[idx]);
+// //   });
+// // }
+// // //
+// // $("ul ").attr("id", "0");
+// // // ASSIGN X,Y CLASSES TO BOARD
+// // // function assignXyLocation(array) {
+// // //   localId = 0;
+// // //   for (var i = 1; i <= array.length; i++) {
+// // //     console.log(array.length);
+// // //     $(".div :nth-child(i)").attr("id", localId);
+// // //     ;
+// // // //     array[i]
+// // // //     // X = 0;
+// // // //     // $(".box").attr("id", X);
+// // // //     // X++;
+// // //   }
+// // //   localId ++;
+// // // //     // Y asix
+// // // //     // Y = 0;
+// // // //     // $(#).append('.' + Y);
+// // // //     // Y++;
+// // // }
+// // // //
+// }
+// //
+// // RUN GAMEBOARD
+// function runGameBoard(array) {
+//   // CREATE BLANK GAME BOARD
+//   createDivRows(array);
+//   createDivBoxes(array);
+//   // COMBINE GAME BOARD + GAME PIECES
+//   singleGamePieceArray = createSingleGamePieceArray(array);
+//   addGamePiecesToHtml(singleGamePieceArray);
+//   // ASSIGN XY ID TO BOXES
+//   // assignXyLocation(gameArray);
+
+// }
+// //
+// // CALL RUN GAME BOARD
+// runGameBoard(gameArray);
+// //
+// //
 // EVENT LISTENERS
 // event listener for each board piece
-$(".box").click(function (e) {
-  e.preventDefault();
-  var localStore = $(this).html();
-  console.log(localStore);
-  $(this).html(currentSwitchPiece);
-  currentSwitchPiece = localStore;
-  $("#switch-piece").html(currentSwitchPiece);
+// $(".box").click(function (e) {
+//   e.preventDefault();
+//   // on click, switch html of switchPiece with html of gameArray
+//   var localStoreHtml = $(this).html();
+//   var localStoreBoardLocation = gameArray.indexOf(this);
+//   console.log(localStoreHtml);
+//   console.log(localStoreBoardLocation);
+//   $(this).html(currentSwitchPiece);
+//   // on click, switch box in gameArray & switchPiece
+//   currentSwitchPiece = localStoreHtml;
+//   $("#switch-piece").html(currentSwitchPiece);
+// });
 
 
-});
-// on click, switch box in gameArray & switchPiece
-// on click, switch html of switchPiece with html of gameArray
 
 
 
